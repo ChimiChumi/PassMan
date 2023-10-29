f5aa8712c2a0cad2cd53577674b49bff8c18dd8a
Hunor Dobos <113592108+ChimiChumi@users.noreply.github.com>
2023-10-29 13:48:16 +0100

    Update configure-path.bat

--- configure-path.bat
+++ configure-path.bat
@@ -1,3 +1,3 @@
-# configurepath.ps1
+# The script below adds the database path dynamically to the environmental variable, which will be used to extract it in the code
 $env:DB_PATH = Get-Location
 Write-Host "Database path set to $env:DB_PATH"
